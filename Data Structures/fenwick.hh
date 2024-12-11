78cfb77ddea152fe3fd68241ef4ee16d9a092260
Semen Babich <sbabich@ptsecurity.com>
2024-12-11 18:43:56 +0300

    Change fenwick.hh

--- Data Structures/fenwick.hh
+++ Data Structures/fenwick.hh
@@ -1,27 +1,22 @@
-namespace FenwickTree {
-struct FenwickTree {
-    vector<int> bit;  // binary indexed tree
-    int n;
-    FenwickTree(int n) {
-        this->n = n;
-        bit.assign(n, 0);
-    }
-    FenwickTree(vector<int> const &a) : FenwickTree(a.size()) {
-        for (size_t i = 0; i < a.size(); i++)
-            add(i, a[i]);
-    }
-    int sum(int r) {
-        int ret = 0;
-        for (; r >= 0; r = (r & (r + 1)) - 1)
-            ret += bit[r];
-        return ret;
-    }
-    int sum(int l, int r) {
-        return sum(r) - sum(l - 1);
-    }
-    void add(int idx, int delta) {
-        for (; idx < n; idx = idx | (idx + 1))
-            bit[idx] += delta;
-    }
+struct FT {
+	vector<ll> s;
+	FT(int n) : s(n) {}
+	void update(int pos, ll dif) { // a[pos] += dif
+		for (; pos < sz(s); pos |= pos + 1) s[pos] += dif;
+	}
+	ll query(int pos) { // sum of values in [0, pos)
+		ll res = 0;
+		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
+		return res;
+	}
+	int lower_bound(ll sum) {// min pos st sum of [0, pos] >= sum
+		// Returns n if no sum is >= sum, or -1 if empty sum is.
+		if (sum <= 0) return -1;
+		int pos = 0;
+		for (int pw = 1 << 25; pw; pw >>= 1) {
+			if (pos + pw <= sz(s) && s[pos + pw-1] < sum)
+				pos += pw, sum -= s[pos-1];
+		}
+		return pos;
+	}
 };
-}
